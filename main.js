0a19c173d24124171bedba75ca998af8227e676a
spar234 <87600450+spar234@users.noreply.github.com>
2021-10-25 15:30:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My first console project");
+
+var frist_name = "Ranbir";
+var last_name = "Kaporr";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+console.log(add);
+
+var sub = a - b;
+console.log(sub);
+
+var multiple = a * b;
+console.log(multiple);
+
+var divide = a / b;
+console.log(divide);
